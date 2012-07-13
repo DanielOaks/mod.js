04ea5e7601eb84262a2874a73d8bb6c4b2c2894d
Nick Markwell <nick@duckinator.net>
2012-07-13 16:30:12 -0400

    Apparently a MIT license was added since I forked jsmodplayer. Using that now.

--- src/decoder.js
+++ src/decoder.js
@@ -1,24 +1,29 @@
 /*
  * mod.js - MOD decoder in JavaScript, using Aurora.js
- * Original (also JavaScript) version from jsmodplayer by Matt Westcott ("Gasman"),
+ * Original JavaScript version from jsmodplayer Copyright (C) 2010 Matt Westcott ("Gasman"),
  * William Wenge-Murphy ("BillyWM"), and Sean Neilan ("sneilan").
  *
- * The original does not have a license specified, so I am releasing it under the ISC license.
- * If any of them object to this, they can contact me directly at <modjs@duckinator.net>.
+ * Aurora.js port by Nick Markwell ("duckinator")
  *
+ * Licensed under the same terms as the original. The original license follows.
  *
- * Copyright (c) 2012, Nick Markwell <nick@duckinator.net>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
  *
- * Permission to use, copy, modify, and/or distribute this software for any purpose
- * with or without fee is hereby granted, provided that the above copyright notice
- * and this permission notice appear in all copies.
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
  *
- * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
- * REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND
- * FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT,
- * OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE,
- * DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS
- * ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  *
  */
 
